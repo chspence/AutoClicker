8b7e87bee00511e2054d39001693e06e1a7a36fd
chspence <curtishpence@gmail.com>
2018-10-31 06:33:56 -0400

    Control and view classes created

--- src/AutoClicker.java
+++ src/AutoClicker.java
@@ -1,9 +1,19 @@
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.InputEvent;
 
 public class AutoClicker {
 
 	public static void main(String[] args) {
-		test2
-		sdffds
+		try {
+			Robot robot = new Robot();
+			robot.delay(3000);
+			robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+	        robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+			
+		} catch (AWTException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
